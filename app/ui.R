90ddff1e7fb3804b5553ca43e94263a2f60eb41c
cameronsevern <cameron.severn@gmail.com>
2021-08-18 16:57:50 -0600

    added example data

--- app/ui.R
+++ app/ui.R
@@ -1,6 +1,5 @@
 library(shiny)
 library(shinyBS)
-
 library(dplyr)
 library(igraph)
 library(DT)
@@ -25,22 +24,22 @@ ui <- function(request) {
                tabPanel("About",
                         h1("About PaIRKAT"),
                         p("PaIRKAT is model framework for assessing statistical relationships between networks of 
-                          metabolites (pathways) and clinical outcome. PaIRKAT queries the KEGG database to determine interactions between metabolites
+                          metabolites (pathways) and some outcome of interest. PaIRKAT queries the KEGG database to determine interactions between metabolites
                           from which network connectivity is constructed."),
                         h3("How to Use"),
                         tags$ol(
                           tags$li("Upload Data",
                                   p("Three datasets are required to use PaIRKAT"),
                                   tags$ol(
-                                    tags$li("Clinical Data: Contains clinical outcomes of interest and any meaningful covariates to be adjusted for. Rows should be subjects and columns should be variables."),
-                                    tags$li("Metabolite Measurements: Contains measurments of metabolites for all subjects. Rows should be subjects and columns should be metabolite names. One column should have subject IDs matching subject IDs in clinical data."),
+                                    tags$li("Phenotype Data: Contains outcomes of interest and any meaningful covariates to be adjusted for. Rows should be subjects and columns should be variables."),
+                                    tags$li("Metabolite Measurements: Contains measurments of metabolites for all subjects. Rows should be subjects and columns should be metabolite names. One column should have subject IDs matching subject IDs in phenotype data."),
                                     tags$li("Pathway Data: Contains linkage data and pathway information. Rows are metabolites and columns are variables. Should contain a column with KEGG IDs.")
                                   ),
                           ),
                           tags$li("Gather Pathways"),
                           p("The Gather Pathways tab will guide you though defining data linkage logic and querying the KEGG database to collect pathway information and form networks of metabolites."),
                           tags$li("Run PaIRKAT"),
-                          p("Define outcome of interest and clinical covaraites to control for. Output from this analysis is a list of significant pathways associated with the outcome of interest."),
+                          p("Define outcome of interest and covaraites to control for. Output from this analysis is a list of significant pathways associated with the outcome of interest."),
                           tags$li("Explore Results"),
                           p("Visual tools to explore the results of PaIRKAT analysis. There are two tools provided with PaIRKAT."),
                           tags$ol(
@@ -52,9 +51,9 @@ ui <- function(request) {
                         h3("Data Cleaning Guide"),
                         p("PaIRKAT requires clean data to function properly. Uploaded files should be in the .csv file format and should abide by the following criteria:"),
                         tags$ul(
-                          tags$li("Rows of clinical data and metabolite measurements with missing data should be removed or imputed."),
+                          tags$li("Rows of phenotype data and metabolite measurements with missing data should be removed or imputed."),
                           tags$li("Metabolite measurements should be normalized."),
-                          tags$li("Metabolite measurements and clinical data should both have a subject ID column with the same name for merging."),
+                          tags$li("Metabolite measurements and phenotype data should both have a subject ID column with the same name for merging."),
                           tags$li("Remove any special characters."),
                           tags$li("Remove duplicate rows or column names."),
                           tags$li("Variables names should be in the first row of the spreadsheet with no leading empty rows prior to the start of the data."),
@@ -122,7 +121,7 @@ ui <- function(request) {
                                      selectizeInput("organism", "Organism", choices = NULL),
                                      helpText('Column in pathway data that contains column names of metabolite data.'),
                                      selectInput("pathCol", "Metabolite Names", choices = NULL),
-                                     helpText('Column in metabolitte measurements and clinical data with subject IDs (should be the same column name in both).'),
+                                     helpText('Column in metabolitte measurements and phenotype data with subject IDs (should be the same column name in both).'),
                                      selectInput("SID", "Subject IDs", choices = NULL),
                                      helpText('Select a minimum pathway size. Pathways with fewer than the minimum size will be filtered from results'),
                                      numericInput("minSize", "Minimum Pathway Size",
@@ -141,7 +140,7 @@ ui <- function(request) {
                           selectInput("Y", "Outcome", choices = NULL),
                           bsTooltip(id = "Y", title = "Select a variable with which to associate metabolites", placement = "bottom", trigger = "hover",
                                     options = NULL),
-                          selectInput("X", "Clinical Covariates", choices = NULL, multiple = T),
+                          selectInput("X", "Adjust for Covariates", choices = NULL, multiple = T),
                           bsTooltip(id = "X", title = "Select all variable to control for", placement = "bottom", trigger = "hover",
                                     options = NULL),
                           radioButtons("outType", "Outcome Type",
@@ -166,7 +165,7 @@ ui <- function(request) {
                         mainPanel(
                           h3("Model Information"),
                           "Outcome: ",textOutput("pKatY"),
-                          "Clinical Covariates: ",textOutput("pKatX"),
+                          "Adjusted Covariates: ",textOutput("pKatX"),
                           h3("Results"),
                           tabsetPanel(
                             tabPanel("Pathway", DT::dataTableOutput("pKatTab")),
@@ -249,7 +248,7 @@ ui <- function(request) {
                                                                                               c(
                                                                                                 "Pathway Results" = "pathways",
                                                                                                 "Metabolite Results" = "metabolites",
-                                                                                                "Clinical/Metabolite Data" = "clinical"
+                                                                                                "Phenotype/Metabolite Data" = "clinical"
                                                                                               )),
                                                                                   selectInput("plotY", "Y Variable",
                                                                                               choices = NULL),
